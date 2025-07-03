ff013f7abc4c67aa0a24475238ee787f6394aeb7
Kozlov AD <kozlovad@mer.ci.nsu.ru>
2025-07-03 16:21:03 +0700

    init commit

---
+++ lua/community.lua
@@ -0,0 +1,19 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  {
+    import = "astrocommunity.colorscheme.kanagawa-nvim",
+    opts = {
+      theme = "lotus", -- Например, для варианта "wave"
+    },
+  },
+
+  import = "astrocommunity.pack.go",
+  -- import/override with your plugins folder
+}
