7d35ee906d27ea3027826be12027d17aad865418
Kozlov Anton <akozlov@axmor.com>
2026-04-27 21:11:46 +0300

    rm unused keybind

--- lua/plugins/ollama.lua
+++ lua/plugins/ollama.lua
@@ -16,13 +16,6 @@ return {
 			mode = { "n", "v" },
 		},
 
-		{
-			"<leader>ai",
-			":<c-u>lua require('ollama').prompt()<cr>",
-			desc = "ollama prompt",
-			mode = { "n", "v" },
-		},
-
 		-- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
 		{
 			"<leader>oG",
